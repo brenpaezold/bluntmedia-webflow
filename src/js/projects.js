73ac5fb5a83638ed7651d464546ca03f9fa31fc9
Brennan Paezold <brennan@bluntmedia.com>
2026-05-29 14:20:40 +0800

    update

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,3 +1,7 @@
+  document.querySelectorAll('.w-condition-invisible').forEach(el => {
+    el.remove();
+  });
+  
 // gsap.registerPlugin(ScrollTrigger);
 
 gsap.set(".project-card", { autoAlpha: 1 });
